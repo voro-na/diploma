4811d5797b29e9db7371e0adb543980bfbbe5f52
voro-na <voropaevaanadya@gmail.com>
2025-04-07 22:40:21 +0300

    delete: old modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { CollectionModule } from './collection/collection.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CorsModule } from './middleware/cors.middleware';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProjectModule } from './project/project.module';
 
@@ -13,11 +10,8 @@ import { ProjectModule } from './project/project.module';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
-    CollectionModule,
     CorsModule,
-    // AuthModule,
-    UsersModule,
     ProjectModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
